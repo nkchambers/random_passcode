6a3d4aa74302ace25b306ea08d592c83475f25f0
nkchambers <nkchames@gmail.com>
2021-12-09 20:36:07 -0800

    added generate passcode method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,10 +8,52 @@ namespace random_passcode.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet("")]
-        public ViewResult Index()
+        private string generatePasscode(int charNum)
         {
+            Random randChar = new Random();
+            string charValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            string result = "";
+
+            for(var i=0; i<charNum; i++)
+            {
+                result += charValues[randChar.Next(charValues.Length)];
+            
+            }
+            
+            return result;
+        }
+
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            if(HttpContext.Session.GetString("passcode") == null)
+                {
+                    HttpContext.Session.SetString("passcode", "Generate a Passcode!");
+                }
+                
+            if(HttpContext.Session.GetInt32("count") == null)
+                {
+                    HttpContext.Session.SetInt32("count", 0);
+                }
+            
+
+            ViewBag.Passcode = HttpContext.Session.GetString("passcode");
+            ViewBag.Count = HttpContext.Session.GetInt32("count");
+
             return View();
+        }
+
+
+        [HttpGet("new_passcode")]
+        public IActionResult NewPasscode()
+        {
+            int? count = HttpContext.Session.GetInt32("count");
+            count++;
+            HttpContext.Session.SetInt32("count", (int)count);
+            HttpContext.Session.SetString("passcode", generatePasscode(14));
+
+            return RedirectToAction("Index");
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace random_passcode
 {
@@ -16,6 +17,8 @@ namespace random_passcode
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession(); //using dependency injection to add session capabilities
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
